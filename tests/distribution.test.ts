755ccab32967a24388e3794511be76f753fa14d1
Pim Pieters <pim@pieters.tv>
2025-11-23 17:21:46 +0100

    Add complex expression test to test accuracy

--- tests/distribution.test.ts
+++ tests/distribution.test.ts
@@ -16,6 +16,49 @@ test('test d20', () => {
   expect(distribution.get(100)).toEqual(0.0);
 });
 
+test('test complex expression', () => {
+  // Results were verified using anydice
+  const expression = '3d6 + 2 * 1d4 - 4';
+  const probabilities: [number, number][] = [
+    [1, 0.0012],
+    [2, 0.0035],
+    [3, 0.0081],
+    [4, 0.015],
+    [5, 0.0255],
+    [6, 0.0394],
+    [7, 0.0544],
+    [8, 0.0706],
+    [9, 0.0845],
+    [10, 0.0961],
+    [11, 0.1019],
+    [12, 0.1019],
+    [13, 0.0961],
+    [14, 0.0845],
+    [15, 0.0706],
+    [16, 0.0544],
+    [17, 0.0394],
+    [18, 0.0255],
+    [19, 0.015],
+    [20, 0.0081],
+    [21, 0.0035],
+    [22, 0.0012],
+  ];
+  const mean = 11.5;
+  const stddev = 3.71;
+
+  const distribution = d20.distribution(expression);
+
+  expect(distribution.min()).toEqual(1);
+  expect(distribution.max()).toEqual(22);
+
+  for (const [key, value] of probabilities) {
+    expect(distribution.get(key)).toBeCloseTo(value, 4);
+  }
+
+  expect(distribution.mean()).toBeCloseTo(mean, 2);
+  expect(distribution.stddev()).toBeCloseTo(stddev, 2);
+});
+
 test('test exceptions', () => {
   expect(() => d20.distribution('1d6 / 0')).toThrowError(d20.DistributionError); // Divide by zero error
   expect(() => d20.distribution('1d6 +')).toThrowError(d20.ParserError); // Invalid expression
